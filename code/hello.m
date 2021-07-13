59f224dc54718d1f3261a75efa89199d7d4c484c
Shangkun Shen <polossk@126.com>
2021-07-13 22:06:48 +0800

    update code color theme

--- code/hello.m
+++ code/hello.m
@@ -1,1 +1,11 @@
-printf('Hello World!')
+function [r, theta] = hello(m, n)
+% hello - Just for test
+%
+% Syntax: [r, theta] = hello(m, n)
+%
+% get random (r, theta) from random points (x, y)
+    printf('Hello World!')
+    x = rand(6, 6); y = rand(6, 6);
+    r = sqrt(a .* a + b .* b);
+    theta = atan2(b, a);
+end
